703cc1b74ef05027b000d395966418a5d0a9f033
Michael Brown <michaelpbrown.007@gmail.com>
2023-10-25 15:15:58 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,31 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3,4};
+    assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1,2};
+    assertArrayEquals(new int[]{2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlace1() {
+    int[] input1 = {};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{}, input1);
+	}
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2,1}, input1);
+	}
+
 }
