ec1595097335c78c6b5a466a93b50d922ae9f0a5
Michael Brown <michaelpbrown.007@gmail.com>
2023-11-05 22:01:31 -0800

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -8,4 +8,17 @@ public void filterList() {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+@Test
+public void filterList2() {
+    int[] input1 = { 3 , 4 , 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5 , 4 , 3 }, input1);
+	}
+@Test
+public void filterList3() {
+    int[] input1 = { };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ }, input1);
+	}
+
 }
